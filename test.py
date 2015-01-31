7d1e1af118651e805657b21cdb820e7c9c6db826
Karan Kamath <karankamath26@gmail.com>
2015-01-30 20:42:58 -0600

    Init

--- test.py
+++ test.py
@@ -1,1 +1,44 @@
-Hello!
+import socket
+import sys
+import constants
+
+def run(user, password, *commands):
+    HOST, PORT = "codebb.cloudapp.net", 17429
+    
+    data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+def subscribe(user, password):
+    HOST, PORT = "codebb.cloudapp.net", 17429
+    
+    data=user + " " + password + "\nSUBSCRIBE\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+def main():
+    run(constants.USER_NAME, constants.PASSWORD, "MY_CASH")
+
+main()
